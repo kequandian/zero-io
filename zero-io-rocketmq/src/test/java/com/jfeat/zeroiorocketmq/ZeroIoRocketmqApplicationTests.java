8a34059b546cc23949100e9a38ffe42227e75518
zuoye <1152808759@qq.com>
2024-06-13 17:51:58 +0800

    补提交配置

---
+++ zero-io-rocketmq/src/test/java/com/jfeat/zeroiorocketmq/ZeroIoRocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfeat.zeroiorocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroIoRocketmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
